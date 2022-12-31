35bb90ec9a0d6dcab882b7f9517a5463f3ff4c53
Max Huckstepp <mhuckstepp@strava.com>
2022-12-30 17:51:59 -0800

    Initial Commit

---
+++ runningRaceCalc/runningRaceCalcApp.swift
@@ -0,0 +1,17 @@
+//
+//  runningRaceCalcApp.swift
+//  runningRaceCalc
+//
+//  Created by Maxim Huckstepp on 12/30/22.
+//
+
+import SwiftUI
+
+@main
+struct runningRaceCalcApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
